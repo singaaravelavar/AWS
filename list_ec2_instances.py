5fa4c2ed9f129d91ba8dde51dfbc17b9437af109
singaaravelavar <59174360+singaaravelavar@users.noreply.github.com>
2022-11-24 22:24:24 +0530

    Create list_ec2_instances.py

---
+++ list_ec2_instances.py
@@ -0,0 +1,16 @@
+import boto3
+
+client = boto3.resource('ec2', 'us-west-2')
+
+def lambda_handler(event, context):
+    
+    instances = client.instances.all()
+    n=0
+    #looping through instances
+    for instance in instances:
+        n=n+1
+        print('Instance number:', n)
+        print('EC2 instance ID:', instance.id)
+        print('Instance state:', instance.state["Name"])
+        print('Instance type:', instance.instance_type)
+        print('-*'*30)
